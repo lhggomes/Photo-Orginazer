2a0613ac3b996fcaf66ff7c8e06611d49aaa5f6e
Lucas Gomes <48351001+lhggomes@users.noreply.github.com>
2020-02-22 22:09:39 -0300

    Creating an function to catch the data

---
+++ photoOrganizer.py
@@ -0,0 +1,16 @@
+import os
+from datetime import datetime
+from PIL import Image
+
+# Crating a Function to Catch the Data of the Photos
+
+
+def photoShootDate(file):
+    photo = Image.open(file)
+    inf = photo._getexif()
+    if 36867 in info:
+        date = info[36867]
+        date = datetime.strptime(date, "%Y:%m:%d %H:%M:%S")
+    else:
+        date = datetime.fromtimestamp(os.path.getmtime(file))
+    return date
